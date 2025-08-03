27f714c1f7eb1a64ab12b36f11765a865c124f4f
Arshan011 <arsha8923@gmail.com>
2025-08-04 03:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (41.18%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,27 +1,42 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
-        if(n == 1){
+
+        if(n==1){
             return nums[0];
         }
-        for(int i = 0; i<n; i++) {
-            if(i == 0){
-            if(nums[i] != nums[i+1]){
-                return nums[i];
+        int st = 0, end = n-1;
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(mid == 0){
+                if(nums[0] != nums[1]){
+                    return nums[mid];
+                }   
+            } 
+            if(mid == n-1){
+                if(nums[n-1] != nums[n-2]){
+                return nums[mid];
+            } 
             }
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];            
+        }
+        if(mid%2==0){               //even case
+            if(nums[mid-1] == nums[mid]){
+                end = mid-1;
+            }else {
+                st = mid+1;
             }
-            else if(i == n-1) {
-                if(nums[i] != nums[i-1]){
-                    return nums[i];
-                }
-                }else{
-                    if(nums[i] != nums[i-1] && nums[i] != nums[i+1]){
-                        return nums[i];
-                    }
-                
-            }
+        }else{             //odd case when mod 2 is not equal to 0
+            if(nums[mid-1] == nums[mid]){
+                st = mid+1;
+            }else {
+                end = mid-1;
+            }        
+        
+         }
+
         }
         return -1;
-        
     }
 }

10f5f66117b5fc2579337156d20ee20257c241d2
Arshan011 <arsha8923@gmail.com>
2025-09-20 02:41:23 +0530

    Time: 7 ms (98.50%) | Memory: 46.6 MB (73.96%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -3,17 +3,16 @@ class Solution {
         if(intervals.length <= 1){
             return intervals;
         }
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
         List<int[]> merged = new ArrayList<>();
-
         for(int[] interval : intervals){
-            if(merged.isEmpty() || merged.get(merged.size()-1)[1] < interval[0]){
+            if(merged.isEmpty()  ||  merged.get(merged.size()-1)[1] < interval[0]){
                 merged.add(interval);
             }else {
-
                 merged.get(merged.size()-1)[1] = Math.max(merged.get(merged.size()-1)[1], interval[1]);
             }
         }
         return merged.toArray(new int[merged.size()][]);
+        
     }
 }

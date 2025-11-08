2500cdb9e73d247f4e561747b93b2052033f7f0a
Arshan011 <arsha8923@gmail.com>
2025-11-08 12:54:36 +0530

    Time: 1 ms (100.00%) | Memory: 48.9 MB (5.51%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,38 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
-        
-        // Ensure nums1 is the smaller array
-        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        if(nums1.length > nums2.length){
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+        int left = 0;
+        int right = nums1.length;
+        while(left <= right){
+            int cut1 = (left + right)/2;
+            int cut2 = (nums1.length + nums2.length +1)/2 - cut1;
 
-        int n = n1 + n2;
-        int left = (n + 1) / 2;
+            int maxleft1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1-1];
+            int minright1 = (cut1 == nums1.length) ? Integer.MAX_VALUE : nums1[cut1];
 
-        int low = 0, high = n1;
-        while (low <= high) {
-            int mid1 = (low + high) / 2;
-            int mid2 = left - mid1;
+            int maxleft2 = (cut2==0) ? Integer.MIN_VALUE : nums2[cut2-1];
+            int minright2 = (cut2 == nums2.length) ? Integer.MAX_VALUE : nums2[cut2];
 
-            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
-            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
-            int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
-            int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
-
-            if (l1 <= r2 && l2 <= r1) {
-                if (n % 2 == 1) {
-                    return Math.max(l1, l2); // Odd length
-                } else {
-                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0; // Even length
+            if(maxleft1 <= minright2  &&  maxleft2 <= minright1){
+                if((nums1.length + nums2.length)%2==0) {
+                    return (double)(Math.max(maxleft1, maxleft2) + Math.min(minright1, minright2))/2;
+                }else{
+                    return (double)Math.max(maxleft1, maxleft2);
                 }
-            } else if (l1 > r2) {
-                high = mid1 - 1;
-            } else {
-                low = mid1 + 1;
+            }else if(maxleft1 > minright2){
+                right = cut1-1;
+            }else{
+                left = cut1+1;
             }
         }
-
-        return 0.0; // This line will never be reached
+        return 0.0;
     }
 }
-        
-    
